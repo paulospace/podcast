7a2b2addc151e43f592ea94c8af4a17573d5481b
Paulo <paulohenprado@gmail.com>
2023-10-23 14:47:04 -0300

    fixed double dispatch

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
@@ -7,9 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/MainRoute.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
